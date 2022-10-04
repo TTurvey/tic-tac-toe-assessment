01ba3aa8ada5178bc7fda0d79fca93b4493a3198
Tim Turvey <tturvey@hotmail.co.uk>
2022-10-04 11:13:17 +0100

    Refactor: equals String type

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,10 +1,6 @@
 public class TicTacToe {
 
     public boolean play(String symbol) {
-        if (symbol == "X") {
-            return true;
-        }
-        return false;
+        return symbol == "X";
     }
-
 }
