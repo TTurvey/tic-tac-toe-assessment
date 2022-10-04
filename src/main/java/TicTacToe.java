0618e23da06d8490563fad4652b6f88769717e68
Tim Turvey <tturvey@hotmail.co.uk>
2022-10-04 11:26:51 +0100

    Green: added allow_X_and_then_O

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,6 +1,17 @@
 public class TicTacToe {
 
+    private int turnNumber = 1;
+
     public boolean play(String symbol) {
-        return symbol == "X";
+
+        if (symbol.equals("X")) {
+            turnNumber++;
+            return true;
+        }
+        if (symbol.equals("O") && turnNumber > 1) {
+            turnNumber++;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -1,10 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TicTacToeShould {
-
 
     @Test
     void
@@ -21,6 +21,16 @@ public class TicTacToeShould {
 
         assertTrue( ticTacToe.play("X") );
     }
+
+    @Test
+    void
+    allow_X_and_then_O() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play("X");
+
+        assertTrue( ticTacToe.play("O") );
+    }
+
 
 
 
